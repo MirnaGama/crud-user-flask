12a62340228aa395f6d124c83685fd8f7d194806
Mirna Gama <mirnabeatrizgamav@gmail.com>
2020-11-02 20:53:45 -0300

    Config para banco SQLite e Mapeamento da tabela User

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # About the project:
 RESTFul API in Flask with User CRUD using Flask-SQLAlchemy
 
+### You should install:
+* Flask
+* Flask SQL Alchemy
+
+### Creating database:
+```bash
+.../api-jwt> python
+>>> from api import db
+>>> db.create_all()
+>>> exit()
+```
+
+### Checking the tables:
+```bash
+.../api-jwt> sqlite3 api.db
+sqlite> .tables
+```
+
 ### Starting the api:
+```bash
 .../crud-user-flask> python api.py
+```
--- api.py
+++ api.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/index', methods=['GET'])
-def hello_world():
-    return "Hello World!"
+###### DATABASE CONFIG
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///api.db' # SQLITE
+db = SQLAlchemy(app)
+
+###### MODEL TABLE FOR USER
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+###### ENDPOINTS BELOW
 
 if __name__ == '__main__':
     app.run(debug=True)
