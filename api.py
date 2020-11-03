c513c7f79231b668dbafa2984a3b5f43b8c81322
Mirna Gama <mirnabeatrizgamav@gmail.com>
2020-11-02 21:08:03 -0300

    Endpoint - CREATE USER

--- api.py
+++ api.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
+# for password hash
+import uuid 
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
@@ -16,7 +20,27 @@ class User(db.Model):
     password = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
 
-###### ENDPOINTS BELOW
+###### ENDPOINTS 
+
+# CREATE USER
+@app.route('/user', methods=['POST'])
+def insert_into_user():
+
+    # get info by request
+    data = request.get_json()
+
+    # creating hash for the password
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    # creating user
+    user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+    
+    # commit into database
+    db.session.add(user)
+    db.session.commit()
+
+    # return a message
+    return jsonify({'message': 'New user has been successfully created!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
